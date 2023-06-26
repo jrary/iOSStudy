fb35f3c535ee22eee449d6f5d566ffb092060658
jrary <jrary@naver.com>
2023-06-27 00:13:17 +0900

    BOJ11021

--- SwiftStudy/BOJ11021/BOJ11021/BOJ11021/main.swift
+++ SwiftStudy/BOJ11021/BOJ11021/BOJ11021/main.swift
@@ -1,6 +1,6 @@
 var N = Int(readLine()!)!
 
-for _ in 1 ... N{
-    var input = readLine()!.split(separator: " ").map{Int($0)!}
-    print(input[0]+input[1])
+for i in 1 ... N{
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    print("Case #\(i):", input[0]+input[1])
 }
