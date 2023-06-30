6654d02c3b725192f88c768a4775b68aaaf62d81
jrary <jrary@naver.com>
2023-06-30 22:23:30 +0900

    BOJ10813

--- SwiftStudy/BOJ10813/BOJ10813/main.swift
+++ SwiftStudy/BOJ10813/BOJ10813/main.swift
@@ -1,11 +1,20 @@
-//
-//  main.swift
-//  BOJ10813
-//
-//  Created by 이다현 on 2023/06/29.
-//
+var I = readLine()!.split(separator: " ")
+let N = Int(I[0])!
+let M = Int(I[1])!
 
-import Foundation
+var pack = [Int](1...N)
+var sw: [[Int]] = []
+for _ in 0...M-1 {
+    var arr = readLine()!.split(separator: " ")
+    sw.append([Int(arr[0])!, Int(arr[1])!])
+}
 
-print("Hello, World!")
+for c in sw{
+    let temp = pack[c[0]-1]
+    pack[c[0]-1] = pack[c[1]-1]
+    pack[c[1]-1] = temp
+}
 
+for i in pack{
+    print(i, terminator: " ")
+}
