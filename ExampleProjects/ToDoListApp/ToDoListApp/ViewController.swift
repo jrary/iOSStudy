e89a7101edf9229dce5f66cea650a7c092e3d4dd
jrary <jrary@naver.com>
2023-07-15 23:59:00 +0900

    [Example1] finished

--- ExampleProjects/ToDoListApp/ToDoListApp/ViewController.swift
+++ ExampleProjects/ToDoListApp/ToDoListApp/ViewController.swift
@@ -65,7 +65,7 @@ extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
         vc.title = "New Task"
         vc.task = tasks[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
